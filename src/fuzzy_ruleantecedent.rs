6759275201b6d19e7cc408b5753b4ec909dc2c17
unknown <etienne.goosse@gmail.com>
2022-02-06 20:07:00 +0100

    maj

--- src/fuzzy_ruleantecedent.rs
+++ src/fuzzy_ruleantecedent.rs
@@ -24,12 +24,33 @@ pub mod fuzzylogic{
     pub struct FuzzyRuleAntecedent{
         op: i32,
         mode: i32,
-        fuzzySet1:FuzzySet,
-        fuzzySet2:FuzzySet,
-        fuzzyRuleAntecedent1: Box<FuzzyRuleAntecedent>,
-        fuzzyRuleAntecedent2: Box<FuzzyRuleAntecedent>,
+        fuzzySet1:Option<FuzzySet>,
+        fuzzySet2:Option<FuzzySet>,
+        fuzzyRuleAntecedent1: Option<Box<FuzzyRuleAntecedent>>,
+        fuzzyRuleAntecedent2: Option<Box<FuzzyRuleAntecedent>>,
     } 
 
+    impl FuzzyRuleAntecedent {
+        pub fn new() -> FuzzyRuleAntecedent {
+            FuzzyRuleAntecedent{
+                op: 0,
+                mode: 0,
+                fuzzySet1:None,
+                fuzzySet2:None,
+                fuzzyRuleAntecedent1: None,
+                fuzzyRuleAntecedent2: None,
+            }
+        }
+        // Method to create a FuzzyRuleAntecedent with just one single FuzzySet
+        pub fn join_single(&mut self, fuzzy_set: FuzzySet)-> bool {
+            return true;
+        }
+
+          // Method to create a FuzzyRuleAntecedent with two FuzzySet, with AND
+        pub fn join_with_and(&mut self, fuzzy_set1: FuzzySet, fuzzy_set2: FuzzySet)-> bool {
+            return true;
+        }
+    }
 
 }
 
@@ -40,7 +61,7 @@ mod tests {
     use crate::fuzzy_set::fuzzylogic::FuzzySet;
 
     #[test]
-    fn test_fuzzy_rule_antecedent() {
+    fn test_new_fuzzy_rule_antecedent() {
         //let  fuzzy_output:FuzzyOutput =  FuzzyOutput::new(1);
         assert_eq!(1, 1);
     }
